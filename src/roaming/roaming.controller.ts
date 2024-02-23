1c918e602ebe9a6ba44595640e01e2f61fc0e9e3
Siwaporn Pensitiporn <>
2024-02-23 11:41:22 +0700

    api roaming map to dtac exsiting

--- src/roaming/roaming.controller.ts
+++ src/roaming/roaming.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { RoamingService } from './roaming.service';
 import { Public } from 'src/auth/decorators/public.decorator';
 
@@ -11,5 +11,30 @@ export class RoamingController {
   @HttpCode(HttpStatus.OK)
   listCountryPackageIR() {
     return this.roamingService.listCountryPackageIR();
+  }
+  //?lang=en&countryCode=JPN&subrType=
+  @Public()
+  @Get('listPackageByCountry?')
+  @HttpCode(HttpStatus.OK)
+  listPackageByCountry(
+    @Query('lang') lang: string,
+    @Query('countryCode') countryCode: string,
+    @Query('subrType') subrType: string,
+  ) {
+    return this.roamingService.listPackageByCountry(
+      lang,
+      countryCode,
+      subrType,
+    );
+  }
+
+  @Public()
+  @Get('getDataRoamingRates?')
+  @HttpCode(HttpStatus.OK)
+  getDataRoamingRates(
+    @Query('lang') lang: string,
+    @Query('countryCode') countryCode: string,
+  ) {
+    return this.roamingService.getDataRoamingRates(lang, countryCode);
   }
 }
